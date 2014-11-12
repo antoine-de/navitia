1d4f602c95207d85a5cdca80506301ea2b984f28
antoine-de <antoine.desbordes@gmail.com>
2014-11-12 16:55:30 +0100

    Disruption: add a last_rt_data_loaded field
    to be able to now the date of the last reload of data due to rt, add a
    field with the datetime of the last rt data integration.
    
    the field is not serialized (I don't see the point)

--- source/jormungandr/jormungandr/interfaces/v1/fields.py
+++ source/jormungandr/jormungandr/interfaces/v1/fields.py
@@ -591,6 +591,7 @@ instance_status = {
         "end_production_date": fields.String(),
         "is_connected_to_rabbitmq": fields.Boolean(),
         "last_load_at": fields.String(),
+        "last_rt_data_loaded": fields.String(),
         "last_load_status": fields.Boolean(),
         "kraken_version": fields.String(attribute="navitia_version"),
         "nb_threads": fields.Integer(),
